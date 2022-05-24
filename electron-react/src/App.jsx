c03786e6d199d1da71441c8f64bf0ec2ecafe0d2
Ali Adel <aliadel56000@gmail.com>
2022-05-24 18:26:30 +0200

    Add loading component

--- electron-react/src/App.jsx
+++ electron-react/src/App.jsx
@@ -1,24 +1,27 @@
-import React, { Fragment, useCallback, useEffect } from 'react';
+import React, { Fragment, useState, useCallback, useEffect } from 'react';
 
-import Input from './components/Input';
+import Input from './components/Input/Input';
+import Loading from './components/Loading/Loading';
 
 const App = () => {
+  const [page, setPage] = useState('input');
+
   useEffect(() => {
     electron.onCompileDone(({ code, compiledCode, symbolTable, errors }) => {
-      console.log('Code', code);
-      console.log('CompiledCode', compiledCode);
-      console.log('SymbolTable', symbolTable);
-      console.log('Errors', errors);
+      setPage('output');
     });
   }, []);
 
   const compile = useCallback((code) => {
+    setPage('loading');
     electron.compile(code);
   });
 
   return (
     <Fragment>
-      <Input compile={compile} />
+      {page === 'input' && <Input compile={compile} />}
+      {page === 'loading' && <Loading />}
+      {page === 'output' && <div>output</div>}
     </Fragment>
   );
 };
---
+++ electron-react/src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './loading.scss';
+
+const Loading = () => {
+  return <div className='loading__container'><div class='lds-dual-ring'></div></div>;
+};
+
+export default Loading;
